c3ee75e3c5f7d9c7807e051abc238cbc0bb93d42
olivia <olivia.bergaglia@gmail.com>
2025-09-18 14:44:35 +0200

    fix effectué

--- src/main/kotlin/system/attr/AttrComponent.kt
+++ src/main/kotlin/system/attr/AttrComponent.kt
@@ -4,6 +4,9 @@ class AttrComponent(var hp: Int) {
 
     fun looseHP(amount: Int) : Boolean{
         hp -= amount;
+        if (hp < 0 ) {
+            hp = 0;
+        }
         // TODO : Empecher négatif (ex hp devient -58)
         return true;
     }
